3d004b439428aea0fa7a282e47858473582a1d54
Apaczer <loaderrr@gmail.com>
2022-08-17 22:39:53 +0200

    revert https://github.com/Apaczer/gmenu2x/commit/c206611e655542396cfbf077de6c5e963311bd2c

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -228,9 +228,7 @@ void GMenu2X::main() {
 		if (prevDateTime > freshDateTime) {
 			set_date_time(prevDateTime.c_str());
 		}
-		else if (!(prevDateTime > freshDateTime)) {
-				build_date_time();
-		}
+
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_JOYSTICK) < 0) {
 		ERROR("Could not initialize SDL: %s", SDL_GetError());
 		quit();
