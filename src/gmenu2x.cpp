8fc1e002e0f5135bc0462c0f708b94f40e7f23e7
Apaczer <94932128+Apaczer@users.noreply.github.com>
2022-12-02 20:33:19 +0100

    add cpu dialog
    in settings

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -677,17 +677,21 @@ void GMenu2X::resetSettings() {
 	}
 }
 
-// void GMenu2X::cpuSettings() {
-// 	SettingsDialog sd(this, ts, tr["CPU settings"], "skin:icons/configure.png");
-// 	sd.addSetting(new MenuSettingInt(this, tr["Default CPU clock"], tr["Set the default working CPU frequency"], &confInt["cpuMenu"], 528, 528, 600, 6));
-// 	sd.addSetting(new MenuSettingInt(this, tr["Maximum CPU clock"], tr["Maximum overclock for launching links"], &confInt["cpuMax"], 740, 600, 1200, 6));
-// 	// sd.addSetting(new MenuSettingInt(this, tr["Minimum CPU clock"], tr["Minimum underclock used in Suspend mode"], &confInt["cpuMin"], 342, 200, 528, 6));
+void GMenu2X::cpuSettings() {
+SettingsDialog sd(this, ts, tr["CPU settings"], "skin:icons/cpu.png");
+sd.allowCancel = true;
+sd.addSetting(new MenuSettingInt(this, tr["Default CPU clock"], tr["Set the default working CPU frequency"], &confInt["cpuMenu"], 672, 672, 864, 96));
+sd.addSetting(new MenuSettingInt(this, tr["Maximum CPU clock"], tr["Maximum overclock for launching links"], &confInt["cpuMax"], 864, 672, 1248, 96));
+sd.addSetting(new MenuSettingInt(this, tr["Minimum CPU clock"], tr["Minimum underclock used in Suspend mode"], &confInt["cpuMin"], 192, 192, 672, 96));
 
-// 	if (sd.exec() && sd.edited() && sd.save) {
-// 		setCPU(confInt["cpuMenu"]);
-// 		writeConfig();
-// 	}
-// }
+
+if (sd.exec() && sd.edited() && sd.save) {
+		setCPU(confInt["cpuMenu"]);
+		setCPU(confInt["cpuMax"]);
+		setCPU(confInt["cpuMin"]);
+ 		writeConfig();
+ 	}
+  }
 
 bool GMenu2X::readTmp() {
 	lastSelectorElement = -1;
